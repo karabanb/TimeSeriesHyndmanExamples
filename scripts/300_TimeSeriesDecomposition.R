bd47bc482b59464a8212785634b25df0311039f4
karabanb <bkarabands@gmail.com>
2020-06-24 10:09:18 +0200

    Chapter was finished

--- scripts/300_TimeSeriesDecomposition.R
+++ scripts/300_TimeSeriesDecomposition.R
@@ -87,9 +87,98 @@ global_economy %>%
   xlab("Year") + ylab("% of GDP") +
   ggtitle("Total Australian exports")
 
+aus_exports <- global_economy %>%
+  filter(Country == "Australia") %>%
+  mutate(
+    `5-MA` = slide_dbl(Exports, mean, .size = 5, .align = "center")
+  )
+
+aus_exports %>%                                   #
+  autoplot(Exports) +
+  autolayer(aus_exports, `5-MA`, color='red') +
+  xlab("Year") + ylab("Exports (% of GDP)") +
+  ggtitle("Total Australian exports") +
+  guides(colour=guide_legend(title="series"))
+
+beer <- aus_production %>%
+  filter(year(Quarter) >= 1992) %>%
+  select(Quarter, Beer)
+
+beer_ma <- beer %>%
+  mutate(
+    `4-MA` = slide_dbl(Beer, mean, .size = 4, .align = "center-left"),
+    `2x4-MA` = slide_dbl(`4-MA`, mean, .size = 2, .align = "center-right")
+  )
+
+## TODO COMMIT ISSUE - need to filter data to reproduce results
+
+us_retail_employment <- us_employment %>%
+  filter(Title == 'Retail Trade') %>%
+  filter_index('1990'~.)
+
+us_retail_employment_ma <- us_retail_employment %>%
+  filter(Title == 'Retail Trade') %>%
+  filter_index('1990'~.) %>% 
+  mutate(
+    `12-MA` = slide_dbl(Employed, mean, .size = 12, .align = "cr"),
+    `2x12-MA` = slide_dbl(`12-MA`, mean, .size = 2, .align = "cl"),
+  )
+
+us_retail_employment_ma %>%
+  autoplot(Employed, color='gray') +
+  autolayer(us_retail_employment_ma, vars(`2x12-MA`), color='red') +
+  xlab("Year") + ylab("Persons (thousands)") +
+  ggtitle("Total employment in US retail")
 
 
+#### CLASSICAL DECOMPOSITION ###########################################################################################
 
+#### Additive ----------------------------------------------------------------------------------------------------------
+
+us_employment %>%
+  filter(Title == 'Retail Trade') %>%
+  filter_index('1990'~.) %>% 
+  model(classical_decomposition(Employed, type = "additive")) -> m
+  components() %>%
+  autoplot() + xlab("Year") +
+  ggtitle("Classical additive decomposition of total US retail employment")
+
+#### Multiplicative ----------------------------------------------------------------------------------------------------  
+  
+us_employment %>%
+  filter(Title == 'Retail Trade') %>%
+  filter_index('1990'~.) %>% 
+  model(classical_decomposition(Employed, type = "mult")) %>%
+  components() %>%
+  autoplot() + xlab("Year") +
+  ggtitle("Classical multiplicative decomposition of total US retail employment")
+
+
+#### X11 Decomposition #################################################################################################
+
+  
+# TODO X11 function isn't implemented in feasts 0.1.4
+  
+# x11_dcmp <- us_retail_employment %>%
+#   model(x11 = feasts::X11(Employed, type = "additive")) %>%
+#   components()
+#   
+# autoplot(x11_dcmp) + xlab("Year") +
+#   ggtitle("Additive X11 decomposition of US retail employment in the US")
+  
+  
+#### SEATS Decomposition ###############################################################################################
+
+# TODO SEATS function isn't implemented in feasts 0.1.4
+
+  
+#### STL Decomposition #################################################################################################
+
+us_retail_employment %>%
+  model(STL(Employed ~ trend(window=7) + season(window='periodic'),
+            robust = TRUE)) %>%
+  components() %>%
+  autoplot()
 
 
 
